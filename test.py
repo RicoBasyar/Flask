c4c27363ebe93ac3ef29dc10c22c6fb75c363d7e
RicoBasyar <RicoBasyar@users.noreply.github.com>
2023-05-13 04:10:25 +0700

    a

--- test.py
+++ test.py
@@ -1,19 +1,17 @@
-# import libraries
+# lib
 import random
 
-# define rules
+# rules
 rules = {
     'do you have a cat': ['Yes, I have a cat.', 'No, I do not have a cat.'],
     'what is your cat\'s name': ['My cat\'s name is Mittens.', 'I do not have a cat.'],
     'what does your cat look like': ['Mittens is a black and white cat with green eyes.']
 }
 
-# define function to respond to user input
+# Fungsi input user
 def respond(input_text):
-    # check each rule for a match
+    # Cek Rule
     for key in rules.keys():
         if key in input_text:
             return random.choice(rules[key])
     return "I'm sorry, I don't understand."
-
-# get user input and respond
